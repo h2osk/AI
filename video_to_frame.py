e7649d7e498e23516284730a065fe99592853b3b
Megha <32072659+meghamishra@users.noreply.github.com>
2020-06-16 12:26:26 -0400

    Add files via upload
    code for downloading videos and extracting frames

---
+++ video_to_frame.py
@@ -0,0 +1,17 @@
+import cv2
+vidcap = cv2.VideoCapture('/home/saumya/summer_2020/smart_kitchen/raw_videos/training/416/Eg89rR5s8e4.mkv')
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("./frames/data_Eg89rR5s8e4/image"+str(count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 1 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
