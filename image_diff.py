3008e158956104fa4bb9f3d8f1d5e56063953a23
Megha <32072659+meghamishra@users.noreply.github.com>
2020-07-12 23:29:37 -0400

    Add files via upload
    frame difference

---
+++ image_diff.py
@@ -0,0 +1,76 @@
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+dim = (480,480)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--first", required=True,
+	help="first input image")
+ap.add_argument("-s", "--second", required=True,
+	help="second")
+args = vars(ap.parse_args())
+
+imageA = cv2.imread(args["first"])
+imageB = cv2.imread(args["second"])
+
+imageA = cv2.resize(imageA,dim,interpolation=cv2.INTER_AREA)
+imageB = cv2.resize(imageB,dim,interpolation=cv2.INTER_AREA)
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 127, 200,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# loop over the contours
+
+max_Area=0
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+
+# for c in cnts:
+# 	# compute the bounding box of the contour and then draw the
+# 	# bounding box on both input images to represent where the two
+# 	# images differ
+# 	(x, y, w, h) = cv2.boundingRect(c)
+# 	area= w*h
+# 	if area>max_Area:
+# 		max_Area=area
+# 		xx,yy,ww,hh=x,y,w,h
+
+
+
+# cv2.rectangle(imageA, (xx, yy), (xx+ ww, yy + hh), (0, 0, 255), 2)
+# cv2.rectangle(imageB, (xx, yy), (xx + ww, yy + hh), (0, 0, 255), 2)
+
+
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
