d3600b09a9381113a5bc4121867067c5f127481e
Megha <32072659+meghamishra@users.noreply.github.com>
2020-07-20 00:48:08 -0400

    Add files via upload
    updated code

--- image_diff.py
+++ image_diff.py
@@ -5,7 +5,7 @@ import imutils
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-dim = (480,480)
+# dim = (480,480)
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--first", required=True,
@@ -17,8 +17,8 @@ args = vars(ap.parse_args())
 imageA = cv2.imread(args["first"])
 imageB = cv2.imread(args["second"])
 
-imageA = cv2.resize(imageA,dim,interpolation=cv2.INTER_AREA)
-imageB = cv2.resize(imageB,dim,interpolation=cv2.INTER_AREA)
+# imageA = cv2.resize(imageA,dim,interpolation=cv2.INTER_AREA)
+# imageB = cv2.resize(imageB,dim,interpolation=cv2.INTER_AREA)
 # convert the images to grayscale
 grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
 grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
@@ -42,30 +42,30 @@ cnts = imutils.grab_contours(cnts)
 # loop over the contours
 
 max_Area=0
-for c in cnts:
-	# compute the bounding box of the contour and then draw the
-	# bounding box on both input images to represent where the two
-	# images differ
-	(x, y, w, h) = cv2.boundingRect(c)
-	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-
-
 # for c in cnts:
 # 	# compute the bounding box of the contour and then draw the
 # 	# bounding box on both input images to represent where the two
 # 	# images differ
 # 	(x, y, w, h) = cv2.boundingRect(c)
-# 	area= w*h
-# 	if area>max_Area:
-# 		max_Area=area
-# 		xx,yy,ww,hh=x,y,w,h
+# 	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# 	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 
 
-# cv2.rectangle(imageA, (xx, yy), (xx+ ww, yy + hh), (0, 0, 255), 2)
-# cv2.rectangle(imageB, (xx, yy), (xx + ww, yy + hh), (0, 0, 255), 2)
+for c in cnts:
+	# compute the bounding box of the contour and then draw the
+	# bounding box on both input images to represent where the two
+	# images differ
+	(x, y, w, h) = cv2.boundingRect(c)
+	area= w*h
+	if area>max_Area:
+		max_Area=area
+		xx,yy,ww,hh=x,y,w,h
+
+
+
+cv2.rectangle(imageA, (xx, yy), (xx+ ww, yy + hh), (0, 0, 255), 2)
+cv2.rectangle(imageB, (xx, yy), (xx + ww, yy + hh), (0, 0, 255), 2)
 
 
 # show the output images
@@ -73,4 +73,9 @@ cv2.imshow("Original", imageA)
 cv2.imshow("Modified", imageB)
 cv2.imshow("Diff", diff)
 cv2.imshow("Thresh", thresh)
+
+cv2.imwrite('Original.jpg',imageA)
+cv2.imwrite('Modified.jpg',imageB)
+cv2.imwrite('Diff.jpg',diff)
+cv2.imwrite('Thresh.jpg',thresh)
 cv2.waitKey(0)
